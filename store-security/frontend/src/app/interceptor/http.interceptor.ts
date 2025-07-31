7f8df627ad7fe2e533dd14c1bae20c4b34c1bfa6
Simone <persolenom@gmail.com>
2025-07-31 07:07:56 +0200

    keycloak

--- store-security/frontend/src/app/interceptor/http.interceptor.ts
+++ store-security/frontend/src/app/interceptor/http.interceptor.ts
@@ -3,36 +3,30 @@ import {
   HttpHeaders,
   HttpInterceptorFn,
 } from '@angular/common/http';
-import { catchError, throwError } from 'rxjs';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { KeycloakService } from 'keycloak-angular'; // importa il servizio keycloak
+import { KeycloakService } from 'keycloak-angular';
 import { SessionStorageService } from '../service/session-storage.service';
+import { catchError, switchMap, throwError, of, from } from 'rxjs';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
   const sessionStorageService = inject(SessionStorageService);
-  const keycloakService = inject(KeycloakService); 
-  let httpHeaders = new HttpHeaders();
+  const keycloakService = inject(KeycloakService);
 
-  keycloakService.getToken().then(token => {
-  console.log("Token reale:", token);
+  return from(keycloakService.getToken()).pipe(
+    switchMap((token) => {
+      let headers = req.headers
+        .set('X-Requested-With', 'XMLHttpRequest');
 
-  let httpHeaders = new HttpHeaders();
-  if (token) {
-    httpHeaders = httpHeaders.append('Authorization', `Bearer ${token}`);
-  }
+      if (token) {
+        headers = headers.set('Authorization', `Bearer ${token}`);
+      }
 
-  });
+      const clonedRequest = req.clone({ headers });
 
-
-  httpHeaders = httpHeaders.append('X-Requested-With', 'XMLHttpRequest');
-
-  const handleHeader = req.clone({
-    headers: httpHeaders,
-  });
-
-  return next(handleHeader).pipe(
+      return next(clonedRequest);
+    }),
     catchError((err: HttpErrorResponse) => {
       if (err.status === 401) {
         sessionStorageService.logout();
